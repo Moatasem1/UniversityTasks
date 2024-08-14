e6ce7be3c835734909625afd5935ed20a9a4b5a0
moatasem <moatasemalnaimat@gmail.com>
2024-08-14 11:18:07 +0300

    link it to front but we need to fix cost

--- minimum_spanning_tree/kruskal.js
+++ minimum_spanning_tree/kruskal.js
@@ -1,158 +1,174 @@
-document.addEventListener("DOMContentLoaded", () => {
 
-    //Disjoint set union
-    class DSU {
-        constructor(size) {
-            this.parent = Array(size + 1).fill(null).map((_, i) => i);
-            this.groupSize = Array(size + 1).fill(1);
-            this.noOfNodes = size;
-        }
-
-        isNodeFound(node) {
-            return node > 0 && node <= this.noOfNodes;
-        }
-
-        getGroupSize(node) {
-            if (!this.isNodeFound(node)) return null;
-            return this.groupSize[this.findParent(node)];
-        }
-
-        findParent(node) {
-            if (!this.isNodeFound(node)) return null;
-
-            if (this.parent[node] !== node) {
-                this.parent[node] = this.findParent(this.parent[node]);
-            }
-            return this.parent[node];
-        }
-
-        inSameSet(node1, node2) {
-            if (!(this.isNodeFound(node1) && this.isNodeFound(node2))) return false;
-            return this.findParent(node1) === this.findParent(node2);
-        }
-
-        union(node1, node2) {
-            if (!(this.isNodeFound(node1) && this.isNodeFound(node2))) return;
-
-            let node1Parent = this.findParent(node1);
-            let node2Parent = this.findParent(node2);
-
-            if (node1Parent === node2Parent) return;
-
-            const [largerGroup, smallerGroup] = this.groupSize[node1Parent] >= this.groupSize[node2Parent]
-                ? [node1Parent, node2Parent] : [node2Parent, node1Parent];
-
-            this.parent[smallerGroup] = largerGroup;
-            this.groupSize[largerGroup] += this.groupSize[smallerGroup];
-        }
+export class DSU {
+    constructor(size) {
+        this.parent = Array(size + 1).fill(null).map((_, i) => i);
+        this.groupSize = Array(size + 1).fill(1);
+        this.noOfNodes = size;
     }
 
-    function getVerticesNumberInGraph(edges) {
-        if (!edges) return 0;
+    isNodeFound(node) {
+        return node > 0 && node <= this.noOfNodes;
+    }
+
+    getGroupSize(node) {
+        if (!this.isNodeFound(node)) return null;
+        return this.groupSize[this.findParent(node)];
+    }
+
+    findParent(node) {
+        if (!this.isNodeFound(node)) return null;
+
+        if (this.parent[node] !== node) {
+            this.parent[node] = this.findParent(this.parent[node]);
+        }
+        return this.parent[node];
+    }
+
+    inSameSet(node1, node2) {
+        if (!(this.isNodeFound(node1) && this.isNodeFound(node2))) return false;
+        return this.findParent(node1) === this.findParent(node2);
+    }
+
+    union(node1, node2) {
+        if (!(this.isNodeFound(node1) && this.isNodeFound(node2))) return;
+
+        let node1Parent = this.findParent(node1);
+        let node2Parent = this.findParent(node2);
+
+        if (node1Parent === node2Parent) return;
+
+        const [largerGroup, smallerGroup] = this.groupSize[node1Parent] >= this.groupSize[node2Parent]
+            ? [node1Parent, node2Parent] : [node2Parent, node1Parent];
+
+        this.parent[smallerGroup] = largerGroup;
+        this.groupSize[largerGroup] += this.groupSize[smallerGroup];
+    }
+}
+
+export class kthMSTs {
+    constructor(edges) {
+        this.edges = edges;
+        this.noOfVertices = this.getVerticesNumberInGraph();
+    }
+
+    getAllMST() {
+        let minimumSpanningTrees = [];
+        let checkedMSTs = new Set();
+        let queue = [];
+
+        let initialMST = this.MST();
+
+        queue.push({ mst: initialMST.spanningTree, totalCost: initialMST.totalCost });
+        checkedMSTs.add(this.getMSTKey(initialMST.spanningTree));
+
+        while (queue.length > 0) {
+            let { mst: currentMST, totalCost } = queue.shift();
+
+            minimumSpanningTrees.push({ currentMST, totalCost });
+
+            this.generateNewMST(currentMST, queue, checkedMSTs);
+        }
+
+        return minimumSpanningTrees.sort((mst1, mst2) => mst1.totalCost - mst2.totalCost);
+    }
+
+    generateNewMST(currentMST, queue, checkedMSTs) {
+        let currentMSTKey = this.getMSTKey(currentMST);
+
+        currentMST.forEach(edgeFromMST => {
+            this.edges.forEach(edgeFromAll => {
+                if (!currentMST.includes(edgeFromAll) &&
+                    this.isValidEdgeReplacement(currentMST, edgeFromMST, edgeFromAll)) {
+
+                    let newMST = currentMST.slice();
+                    this.removeEdgeFromArray(newMST, edgeFromMST);
+                    newMST.push(edgeFromAll);
+
+                    let newMSTKey = this.getMSTKey(newMST);
+                    if (!checkedMSTs.has(newMSTKey)) {
+                        queue.push({ mst: newMST, totalCost: newMST.reduce((sum, edge) => sum + edge.weight, 0) });
+                        checkedMSTs.add(newMSTKey);
+                    }
+                }
+            });
+        });
+    }
+
+    getMSTKey(mst) {
+        return mst.map(edge => [edge.vertex1, edge.vertex2].sort().toString()).sort().join(';');
+    }
+
+    MST() {
+        this.edges.sort((edge1, edge2) => edge1.weight - edge2.weight);
+        let dsuEdges = new DSU(this.noOfVertices);
+        let spanningTree = [];
+        let totalCost = 0;
+
+        for (const edge of this.edges) {
+
+            if (!dsuEdges.inSameSet(edge.vertex1, edge.vertex2)) {
+                spanningTree.push(edge);
+                totalCost += edge.weight;
+                dsuEdges.union(edge.vertex1, edge.vertex2);
+            }
+
+            if (spanningTree.length === this.noOfVertices - 1)
+                break;
+        }
+
+        return { spanningTree, totalCost };
+    }
+
+    getVerticesNumberInGraph() {
+        if (!this.edges) return 0;
 
         const vertices = new Set();
-        edges.forEach(edge => {
+        this.edges.forEach(edge => {
             vertices.add(edge.vertex1);
             vertices.add(edge.vertex2);
         });
 
-        //in case user input have in non sequentail order
+        // in case user input has non-sequential order
         return Math.max(...vertices);
     }
 
-    function getMSTEdgesUseKruskal(edges) {
-        let mstEdges = [];
+    removeEdgeFromArray(array, edgeToRemove) {
+        let removeIndex = array.findIndex(edge =>
+            edge.vertex1 === edgeToRemove.vertex1 && edge.vertex2 === edgeToRemove.vertex2
+        );
 
-        if (!edges || edges.length === 0) return mstEdges;
-
-        // Sort edges by weight in ascending order
-        edges.sort((edge1, edge2) => edge1.weight - edge2.weight);
-
-        const dsuEdges = new DSU(getVerticesNumberInGraph(edges));
-
-        edges.forEach(edge => {
-            if (!dsuEdges.inSameSet(edge.vertex1, edge.vertex2)) {
-                mstEdges.push(edge);
-                dsuEdges.union(edge.vertex1, edge.vertex2);
-            }
-        });
-
-        console.log(mstEdges);
-        return mstEdges;
+        if (removeIndex !== -1) {
+            array.splice(removeIndex, 1);
+        }
     }
 
-    function getEdgestAsCytoscapeData(edges) {
-        let elements = [];
+    isSpanningTreeCycles(spanningTree) {
+        let dsu = new DSU(this.noOfVertices);
 
-        edges.forEach(edge => {
-            elements.push({ data: { id: edge.vertex1 } });
-            elements.push({ data: { id: edge.vertex2 } });
-        });
-        edges.forEach(edge => {
-            elements.push({ data: { source: edge.vertex1, target: edge.vertex2, weight: edge.weight } });
-        });
+        for (let edge of spanningTree) {
+            if (dsu.inSameSet(edge.vertex1, edge.vertex2)) return false; // there is a cycle
 
-        return elements;
+            dsu.union(edge.vertex1, edge.vertex2);
+        }
+
+        return true;
     }
 
-    function drawGraph(containerId, edges) {
-        return cytoscape({
-            container: document.getElementById(containerId),
-            elements: getEdgestAsCytoscapeData(edges),
-            style: [
-                {
-                    selector: 'node',
-                    style: {
-                        'background-color': '#3d52a0',
-                        'color': 'white',
-                        'label': 'data(id)',
-                        'text-valign': 'center',
-                        'text-halign': 'center',
-                        'font-size': '14px'
-                    }
-                },
-                {
-                    selector: 'edge',
-                    style: {
-                        'width': 1.6,
-                        'line-color': '#8697c3',
-                        'target-arrow-color': '#8697c3',
-                        'target-arrow-shape': 'triangle',
-                        'label': 'data(weight)',
-                        'color': '#7191e6',
-                        'font-size': '14px'
-                    }
-                }
-            ]
-
-            ,
-            layout: {
-                name: 'grid'
-            }
-        });
+    isValidEdgeReplacement(currentMST, edgeToRemove, edgeToAdd) {
+        let newMST = currentMST.slice();
+        this.removeEdgeFromArray(newMST, edgeToRemove);
+        newMST.push(edgeToAdd);
+        return this.isSpanningTreeCycles(newMST);
     }
+}
 
-    function getTextAreaInputAsEdges(textAreaInput) {
-        let edges = [];
-        let lines = textAreaInput.trim().split('\n');
-        lines.forEach(line => {
-            const [v1, v2, w] = line.trim().split(/\s+/);
-            edges.push({ vertex1: v1, vertex2: v2, weight: w });
-        });
+// let edges = [
+//     { vertex1: 1, vertex2: 2, weight: 1 },
+//     { vertex1: 2, vertex2: 3, weight: 2 },
+//     { vertex1: 3, vertex2: 4, weight: 4 },
+//     { vertex1: 4, vertex2: 1, weight: 3 },
+//     { vertex1: 1, vertex2: 3, weight: 2 },
+//     { vertex1: 2, vertex2: 4, weight: 3 }
+// ];
 
-        return edges;
-    }
-
-    let generateMSTBtn = document.getElementById("generateMSTBtn");
-    let graphsWrapper = document.getElementById("graphsWrapper");
-    let graphInput = document.getElementById("graphInput");
-
-    generateMSTBtn.addEventListener("click", () => {
-        let edges = getTextAreaInputAsEdges(graphInput.value);
-        graphsWrapper.classList.remove("d-none");
-        graphsWrapper.scrollIntoView({ behavior: 'smooth', block: 'start' });
-        drawGraph("orginalGraph", edges);
-        drawGraph("mstGraph", getMSTEdgesUseKruskal(edges));
-    });
-
-});
+//https://www.geeksforgeeks.org/spanning-tree/
--- minimum_spanning_tree/main.js
+++ minimum_spanning_tree/main.js
@@ -1,179 +1,102 @@
+import { DSU, kthMSTs } from './kruskal.js';
+
 document.addEventListener("DOMContentLoaded", () => {
-    class DSU {
-        constructor(size) {
-            this.parent = Array(size + 1).fill(null).map((_, i) => i);
-            this.groupSize = Array(size + 1).fill(1);
-            this.noOfNodes = size;
-        }
 
-        isNodeFound(node) {
-            return node > 0 && node <= this.noOfNodes;
-        }
+    function getEdgestAsCytoscapeData(edges) {
+        let elements = [];
 
-        getGroupSize(node) {
-            if (!this.isNodeFound(node)) return null;
-            return this.groupSize[this.findParent(node)];
-        }
+        edges.forEach(edge => {
+            elements.push({ data: { id: edge.vertex1 } });
+            elements.push({ data: { id: edge.vertex2 } });
+        });
+        edges.forEach(edge => {
+            elements.push({ data: { source: edge.vertex1, target: edge.vertex2, weight: edge.weight } });
+        });
 
-        findParent(node) {
-            if (!this.isNodeFound(node)) return null;
-
-            if (this.parent[node] !== node) {
-                this.parent[node] = this.findParent(this.parent[node]);
-            }
-            return this.parent[node];
-        }
-
-        inSameSet(node1, node2) {
-            if (!(this.isNodeFound(node1) && this.isNodeFound(node2))) return false;
-            return this.findParent(node1) === this.findParent(node2);
-        }
-
-        union(node1, node2) {
-            if (!(this.isNodeFound(node1) && this.isNodeFound(node2))) return;
-
-            let node1Parent = this.findParent(node1);
-            let node2Parent = this.findParent(node2);
-
-            if (node1Parent === node2Parent) return;
-
-            const [largerGroup, smallerGroup] = this.groupSize[node1Parent] >= this.groupSize[node2Parent]
-                ? [node1Parent, node2Parent] : [node2Parent, node1Parent];
-
-            this.parent[smallerGroup] = largerGroup;
-            this.groupSize[largerGroup] += this.groupSize[smallerGroup];
-        }
+        return elements;
     }
 
-    class kthMSTs {
-        constructor(edges) {
-            this.edges = edges;
-            this.noOfVertices = this.getVerticesNumberInGraph();
-        }
-
-        getAllMST() {
-            let minimumSpanningTrees = [];
-            let checkedMSTs = new Set();
-            let queue = [];
-
-            let initialMST = this.MST();
-
-            queue.push({ mst: initialMST.spanningTree, totalCost: initialMST.totalCost });
-            checkedMSTs.add(this.getMSTKey(initialMST.spanningTree));
-
-            while (queue.length > 0) {
-                let { mst: currentMST, totalCost } = queue.shift();
-
-                minimumSpanningTrees.push({ currentMST, totalCost });
-
-                this.generateNewMST(currentMST, queue, checkedMSTs);
-            }
-
-            return minimumSpanningTrees.sort((mst1, mst2) => mst1.totalCost - mst2.totalCost);
-        }
-
-        generateNewMST(currentMST, queue, checkedMSTs) {
-            let currentMSTKey = this.getMSTKey(currentMST);
-
-            currentMST.forEach(edgeFromMST => {
-                this.edges.forEach(edgeFromAll => {
-                    if (!currentMST.includes(edgeFromAll) &&
-                        this.isValidEdgeReplacement(currentMST, edgeFromMST, edgeFromAll)) {
-
-                        let newMST = currentMST.slice();
-                        this.removeEdgeFromArray(newMST, edgeFromMST);
-                        newMST.push(edgeFromAll);
-
-                        let newMSTKey = this.getMSTKey(newMST);
-                        if (!checkedMSTs.has(newMSTKey)) {
-                            queue.push({ mst: newMST, totalCost: newMST.reduce((sum, edge) => sum + edge.weight, 0) });
-                            checkedMSTs.add(newMSTKey);
-                        }
+    function drawGraph(containerId, edges) {
+        return cytoscape({
+            container: document.getElementById(containerId),
+            elements: getEdgestAsCytoscapeData(edges),
+            style: [
+                {
+                    selector: 'node',
+                    style: {
+                        'background-color': '#3d52a0',
+                        'color': 'white',
+                        'label': 'data(id)',
+                        'text-valign': 'center',
+                        'text-halign': 'center',
+                        'font-size': '14px'
                     }
-                });
-            });
-        }
-
-        getMSTKey(mst) {
-            return mst.map(edge => [edge.vertex1, edge.vertex2].sort().toString()).sort().join(';');
-        }
-
-        MST() {
-            this.edges.sort((edge1, edge2) => edge1.weight - edge2.weight);
-            let dsuEdges = new DSU(this.noOfVertices);
-            let spanningTree = [];
-            let totalCost = 0;
-
-            for (const edge of this.edges) {
-
-                if (!dsuEdges.inSameSet(edge.vertex1, edge.vertex2)) {
-                    spanningTree.push(edge);
-                    totalCost += edge.weight;
-                    dsuEdges.union(edge.vertex1, edge.vertex2);
+                },
+                {
+                    selector: 'edge',
+                    style: {
+                        'width': 1.6,
+                        'line-color': '#8697c3',
+                        'target-arrow-color': '#8697c3',
+                        'target-arrow-shape': 'triangle',
+                        'label': 'data(weight)',
+                        'color': '#7191e6',
+                        'font-size': '14px'
+                    }
                 }
+            ]
 
-                if (spanningTree.length === this.noOfVertices - 1)
-                    break;
+            ,
+            layout: {
+                name: 'grid'
             }
-
-            return { spanningTree, totalCost };
-        }
-
-        getVerticesNumberInGraph() {
-            if (!this.edges) return 0;
-
-            const vertices = new Set();
-            this.edges.forEach(edge => {
-                vertices.add(edge.vertex1);
-                vertices.add(edge.vertex2);
-            });
-
-            // in case user input has non-sequential order
-            return Math.max(...vertices);
-        }
-
-        removeEdgeFromArray(array, edgeToRemove) {
-            let removeIndex = array.findIndex(edge =>
-                edge.vertex1 === edgeToRemove.vertex1 && edge.vertex2 === edgeToRemove.vertex2
-            );
-
-            if (removeIndex !== -1) {
-                array.splice(removeIndex, 1);
-            }
-        }
-
-        isSpanningTreeCycles(spanningTree) {
-            let dsu = new DSU(this.noOfVertices);
-
-            for (let edge of spanningTree) {
-                if (dsu.inSameSet(edge.vertex1, edge.vertex2)) return false; // there is a cycle
-
-                dsu.union(edge.vertex1, edge.vertex2);
-            }
-
-            return true;
-        }
-
-        isValidEdgeReplacement(currentMST, edgeToRemove, edgeToAdd) {
-            let newMST = currentMST.slice();
-            this.removeEdgeFromArray(newMST, edgeToRemove);
-            newMST.push(edgeToAdd);
-            return this.isSpanningTreeCycles(newMST);
-        }
+        });
     }
 
-    let edges = [
-        { vertex1: 1, vertex2: 2, weight: 1 },
-        { vertex1: 2, vertex2: 3, weight: 2 },
-        { vertex1: 3, vertex2: 4, weight: 4 },
-        { vertex1: 4, vertex2: 1, weight: 3 },
-        { vertex1: 1, vertex2: 3, weight: 2 },
-        { vertex1: 2, vertex2: 4, weight: 3 }
-    ];
+    function getTextAreaInputAsEdges(textAreaInput) {
+        let edges = [];
+        let lines = textAreaInput.trim().split('\n');
+        lines.forEach(line => {
+            const [v1, v2, w] = line.trim().split(/\s+/);
+            edges.push({ vertex1: v1, vertex2: v2, weight: w });
+        });
 
-    let MSTs = new kthMSTs(edges);
+        return edges;
+    }
 
-    console.log(MSTs.getAllMST());
+    function drawAllGraphs(MSTs) {
+        let coutner = 1;
+        MSTs.forEach(mst => {
+            createGraphContainerEl(coutner, mst.totalCost);
+            drawGraph(coutner, mst.currentMST);
+            coutner++;
+        });
+    }
+
+    function createGraphContainerEl(id, cost) {
+        let graphContainerEl = document.createElement('div');
+
+        graphContainerEl.innerHTML = `<div class="mt-5">
+                                            <h3 class="fw-semibold">Spanning Tree ${id} - Cost (${cost}):</h3>
+                                            <div class="rounded graph-wrapper" id="${id}"></div>
+                                        </div>`;
+
+        document.getElementById("graphsWrapper").appendChild(graphContainerEl);
+    }
+
+    let generateMSTBtn = document.getElementById("generateMSTBtn");
+    let graphsWrapper = document.getElementById("graphsWrapper");
+    let graphInput = document.getElementById("graphInput");
+
+    generateMSTBtn.addEventListener("click", () => {
+
+        let edges = getTextAreaInputAsEdges(graphInput.value);
+        graphsWrapper.classList.remove("d-none");
+        graphsWrapper.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        drawGraph("orginalGraph", edges);
+        let MSTs = new kthMSTs(edges);
+        console.log(MSTs.getAllMST());
+        drawAllGraphs(MSTs.getAllMST());
+    });
+
 });
-
-//https://www.geeksforgeeks.org/spanning-tree/
